3a9df662ffe0900703adee150562ca3561a1f73c
DarwinB <92453085+DarwinBG@users.noreply.github.com>
2021-10-29 14:07:12 -0500

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+float x,y,z;
+cout<<"Ingresar 2 numeros x y y :";
+cin>>x>>y;
+cout<<"El resutaldo es :";
+z=x+y;
+cout<<z<<endl;
+return 0;
+}
+
