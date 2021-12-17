36563731d5f3f566cd7778f8995b14a53aa10b6c
DarwinBG <92453085+DarwinBravoGranda@users.noreply.github.com>
2021-12-17 09:06:42 -0500

    ejercicios

---
+++ Examen c2/Darwin Bravo/promedio notas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+ 
+ 
+ 
+int main () 
+{
+	float promedio;  
+		printf("INGRESE EL  PROMEDIo: "); 
+		scanf("%f", &promedio); 
+	
+	if(promedio>=7 )
+		{
+		printf("Aprobado");
+		}
+		else
+		{
+		printf ("Reprobado");
+    	}
+    	getch();
+  }
+	
+	
+	
