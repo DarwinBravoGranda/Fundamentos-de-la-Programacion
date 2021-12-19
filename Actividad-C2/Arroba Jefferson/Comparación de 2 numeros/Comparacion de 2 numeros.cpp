8e280d1fbb6b9762ceb1c431035c5e1c339b66ed
JeffersonArroba <88253109+JeffersonArroba@users.noreply.github.com>
2021-12-18 21:37:22 -0500

    Actividad C2-

---
+++ Actividad-C2/Arroba Jefferson/Comparación de 2 numeros/Comparacion de 2 numeros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float n1,n2;
+	if(n1=n2){
+		cout<<"El Valor de n1: "<<n1<<"Es igual a n2: "<<n2<<endl;
+	}else{
+		if(n1<n2){
+			cout<<"El Valor de n1: "<<n1<<"Es menor a n2: "<<n2<<endl;
+		}else{
+			cout<<"El Valor de n1: "<<n2<<"Es menor a n1: "<<n1<<endl;
+		}
+	}
+	return 0;
+}
