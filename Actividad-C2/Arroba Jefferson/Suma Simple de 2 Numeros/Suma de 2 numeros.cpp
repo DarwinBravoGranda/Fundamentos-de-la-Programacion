7314d6dd0fabb4cf2e767cd8d29222cb4cbc6e02
JeffersonArroba <88253109+JeffersonArroba@users.noreply.github.com>
2021-12-18 21:36:13 -0500

    Actividad C2-

---
+++ Actividad-C2/Arroba Jefferson/Suma Simple de 2 Numeros/Suma de 2 numeros.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float n1,n2,r;
+cout<<"Ingrese el Primer Numero : ";
+cin>>n1;
+cout<<"Ingrese el Segundo Numero : ";	
+cin>>n2;
+r=n1+n2;
+cout<<"El Resultado de la Suma de ambos numeros es : "<<r<<endl;
+return 0;	
+}
